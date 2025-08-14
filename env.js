979cde5f7a63f693521c8af89b56c5395f72422b
tejas-sat <tejass@internetsoft.com>
2025-08-14 15:46:54 +0530

    form submissions

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 export const REACT_APP_API_BASE_URL = "http://localhost:5000";
-// export const BACKEND_URL =
-//   "https://dev.franchiselistings.com/franchise_backend";
-export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL =
+  "https://dev.franchiselistings.com/franchise_backend";
+// export const BACKEND_URL = "http://localhost:5000";
